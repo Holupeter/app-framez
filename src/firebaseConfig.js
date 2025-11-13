fc41cfb8c7e9316f03ff0ba050493a46ffa4939f
Orion <holupeter2@gmail.com>
2025-11-13 05:01:33 +0100

    fix: clean config code

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -19,14 +19,15 @@ if (!__DEV__) {
   }
 
   console.log("Firebase secrets found. Initializing...");
-  firebaseConfig = {
-    apiKey: process.env.FIREBASE_API_KEY,
-    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.FIREBASE_PROJECT_ID,
-    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.FIREBASE_APP_ID,
-  };
+// Replace this with your actual Firebase config object from the Firebase console
+const firebaseConfig = {
+  apiKey: "AIzaSyD6PGWoVaj8NmdN9PSdhaUQ2i7C4xl5UwM",
+  authDomain: "framezapp-78fcd.firebaseapp.com",
+  projectId: "framezapp-78fcd",
+  storageBucket: "framezapp-78fcd.firebasestorage.app",
+  messagingSenderId: "28874798727",
+  appId: "1:28874798727:web:2721e4c75da2d2ca6bea22"
+};
 
 } else {
   // LOCAL DEVELOPMENT
